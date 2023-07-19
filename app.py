db0cd0fdcde9843b95629a060e2e7c41d9f4fb20
AMCalejandro <alejandro.carrasco.20@ucl.ac.uk>
2023-07-19 09:33:49 -0400

    Add st.ses_state item to move df across tabs

--- app.py
+++ app.py
@@ -4,8 +4,11 @@ from apps import home, data_checking, uploadSystem
 
 st.set_page_config(layout="wide")
 
-app = MultiApp()
+# Initialise dfqc in session state to move across tabs
+if 'dfqc' not in st.session_state:
+    st.session_state['dfqc'] = 'NULL'
 
+app = MultiApp()
 st.markdown(""" 
 # GP2 clinical cohort self checking and upload system
 """
@@ -14,7 +17,5 @@ st.markdown("""
 app.add_app("Home", home.app)
 app.add_app("QC", data_checking.app)
 app.add_app("Upload", uploadSystem.app)
-
 # Run the main app
 app.run()
-
