f107973d4be8840010aeccf40508246f8492ee5e
AMCalejandro <alejandro.carrasco.20@ucl.ac.uk>
2023-07-19 09:45:37 -0400

    Key change caching functions df accessing GCP

--- utils/generategp2ids.py
+++ utils/generategp2ids.py
@@ -1,13 +1,19 @@
 import json
+import ijson
 import numpy as np
 import pandas as pd
 from google.cloud import storage
+import streamlit as st
 
-def update_masterids(blob, ids_log, ids_dict):
+@st.cache
+def update_masterids(ids_log, ids_dict):
+    client = storage.Client()
+    bucket = client.get_bucket('eu-samplemanifest')
+    blob = bucket.blob('IDSTRACKER/CLINICALGP2IDS_MAPPER_20230718.json')
+    
     with blob.open("r") as fp:
         masterids = json.load(fp)
     
-    #if 'ids_tracker' in globals():
     if bool(ids_dict):
         for study, newdata in ids_log.items():
             masterids[study].update(newdata)
@@ -19,6 +25,21 @@ def update_masterids(blob, ids_log, ids_dict):
         json.dump(masterids, fp)
     
     print("MASTER IDS UPDATED")
+
+@st.cache
+def master_key(studies):
+    # ACCESS MASTERGP2IDS_JSON IN GP2 BUCKET
+    client = storage.Client()
+    bucket = client.get_bucket('eu-samplemanifest')
+    blob = bucket.blob('IDSTRACKER/CLINICALGP2IDS_MAPPER_20230718.json')
+    
+    ids_tracker = {}
+    with blob.open("r") as f:
+        for cohort in studies:
+            for record in ijson.items(f, cohort):
+                ids_tracker.update({cohort : record})
+    
+    return(ids_tracker)
 
 # CONVERT THIS INTO A FUNCTIONS
 # INPUT
@@ -40,3 +61,45 @@ def getgp2ids(df_updateids, n, uids, study_code):
     return(mydf)
 
 
+
+# @st.cache
+# def blobbucket(bname = 'eu-samplemanifest'):
+#     client = storage.Client()
+#     bucket = client.get_bucket(bname)
+#     blob = bucket.blob('IDSTRACKER/CLINICALGP2IDS_MAPPER_20230718.json')
+#     return blob
+
+# @st.cache
+# def update_masterids(blob, ids_log, ids_dict):
+#     #client = storage.Client()
+#     #bucket = client.get_bucket('eu-samplemanifest')
+#     #blob = bucket.blob('IDSTRACKER/CLINICALGP2IDS_MAPPER_20230718.json')
+#     with blob.open("r") as fp:
+#         masterids = json.load(fp)
+    
+#     #if 'ids_tracker' in globals():
+#     if bool(ids_dict):
+#         for study, newdata in ids_log.items():
+#             masterids[study].update(newdata)
+#     else:
+#         for study, newdata in ids_log.items():
+#             masterids[study] = newdata
+    
+#     with blob.open("w") as fp:
+#         json.dump(masterids, fp)
+    
+#     print("MASTER IDS UPDATED")
+
+# @st.cache
+# def master_key(blob, studies):
+#     # ACCESS MASTERGP2IDS_JSON IN GP2 BUCKET
+#     #client = storage.Client()
+#     #bucket = client.get_bucket('eu-samplemanifest')
+#     #blob = bucket.blob('IDSTRACKER/CLINICALGP2IDS_MAPPER_20230718.json')
+#     ids_tracker = {}
+#     with blob.open("r") as f:
+#         for cohort in studies:
+#             st.write("IN")
+#             for record in ijson.items(f, cohort):
+#                 ids_tracker.update({cohort : record})
+#     return(ids_tracker)
