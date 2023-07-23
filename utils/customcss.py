745fefde82fb7b55fcd25d70e6d1d7c2ecd7b4e1
AMCalejandro <alejandro.carrasco.20@ucl.ac.uk>
2023-07-23 17:03:09 -0400

    Remove args from load_css

--- utils/customcss.py
+++ utils/customcss.py
@@ -1,4 +1,5 @@
 import streamlit as st
-def load_css(file_name = "/home/amcalejandro/Data/WorkingDirectory/Development_Stuff/GP2_SAMPLE_UPLOADER/sample_uploader/apps/css/css.css"):
+
+def load_css(file_name):
    with open(file_name) as f:
       st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
