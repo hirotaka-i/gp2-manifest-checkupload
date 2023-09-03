508f4d87256011d4b02ccd338963a93edd4392e6
AMCalejandro <alejandro.carrasco.20@ucl.ac.uk>
2023-09-03 16:03:54 -0400

    Add improved master key and gp2ids functions to detect dup clinical ids

--- utils/generategp2ids.py
+++ utils/generategp2ids.py
@@ -7,7 +7,7 @@ import streamlit as st
 
 
 #@st.cache(hash_funcs={'_json.Scanner': hash})
-@st.experimental_memo()
+#@st.experimental_memo()
 def update_masterids(ids_log, ids_dict):
     client = storage.Client()
     bucket = client.get_bucket('eu-samplemanifest')
@@ -32,38 +32,79 @@ def update_masterids(ids_log, ids_dict):
     #print("MASTER IDS UPDATED")
 
 
-@st.cache
+#@st.cache
 def master_key(studies):
     # ACCESS MASTERGP2IDS_JSON IN GP2 BUCKET
     client = storage.Client()
     bucket = client.get_bucket('eu-samplemanifest')
-    #blob = bucket.blob('IDSTRACKER/CLINICALGP2IDS_MAPPER_20230718.json')
-    #blob = bucket.blob('IDSTRACKER/CLINICALGP2IDS_MAPPER_20230810.json')
     blob = bucket.blob('IDSTRACKER/GP2IDSMAPPER.json')
     
     ids_tracker = {}
-    with blob.open("r") as f:
-        for cohort in studies:
+    for cohort in studies:
+        with blob.open("r") as f:
+        #for cohort in studies:
             for record in ijson.items(f, cohort):
                 ids_tracker.update({cohort : record})
     
     return(ids_tracker)
 
+def master_keyv2(studies):
+    # ACCESS MASTERGP2IDS_JSON IN GP2 BUCKET
+    client = storage.Client()
+    bucket = client.get_bucket('eu-samplemanifest')
+    blob = bucket.blob('IDSTRACKER/GP2IDSMAPPER.json')
+    
+    ids_tracker = {}
+    with blob.open("r") as f:
+        for k, v in ijson.kvitems(f, ''):
+            if k in studies:
+                ids_tracker.update({k:v})
+    
+    return(ids_tracker)
 
-def getgp2ids(df_updateids, n, uids, study_code):
+
+
+def getgp2idsv2(dfproc, n, study_code):
+    df_dups = dfproc[dfproc.duplicated(keep=False, subset=['clinical_id'])].sort_values('clinical_id').reset_index(drop = True).copy()
+    if df_dups.shape[0]>0:
+        dupids_mapper = dict(zip(df_dups.clinical_id.unique(),
+                            [num+n for num in range(len(df_dups.clinical_id.unique()))]))
+        
+        df_dup_chunks = []
+        for clin_id, gp2id in dupids_mapper.items():
+            df_dups_subset = df_dups[df_dups.clinical_id==clin_id].copy()
+            df_dups_subset['GP2ID'] = [f'{study_code}_{gp2id:06}' for i in range(df_dups_subset.shape[0])]
+            df_dups_subset['SampleRepNo'] = ['s'+str(i+1) for i in range(df_dups_subset.shape[0])]
+            df_dups_subset['GP2sampleID'] = df_dups_subset['GP2ID'] + '_' + df_dups_subset['SampleRepNo']
+            df_dup_chunks.append(df_dups_subset)
+        df_dups_wids = pd.concat(df_dup_chunks)
+
+    df_nodups = dfproc[~dfproc.duplicated(keep=False, subset=['clinical_id'])].sort_values('clinical_id').reset_index(drop = True).copy()
+
+    if df_dups.shape[0]>0:
+        n =  len(list(dupids_mapper.values())) + n
+    else:
+        n = n
+
+    uids = [str(id) for id in df_nodups['sample_id'].unique()]
     mapid = {}
     for uid in uids:
         mapid[uid]= n
         n += 1
-    mydf = df_updateids.copy()
-    mydf['uid_idx'] = mydf['sample_id'].map(mapid)
-    mydf['GP2ID'] = [f'{study_code}_{i:06}' for i in mydf.uid_idx]
-    mydf['uid_idx_cumcount'] = mydf.groupby('GP2ID').cumcount() + 1
-    mydf['GP2sampleID'] = mydf.GP2ID + '_s' + mydf.uid_idx_cumcount.astype('str')
-    mydf['SampleRepNo'] = 's' + mydf.uid_idx_cumcount.astype('str')
-    mydf.drop(['uid_idx','uid_idx_cumcount'], axis = 1, inplace = True)
-    return(mydf)
+    df_nodups_wids = df_nodups.copy()
+    df_nodups_wids['uid_idx'] = df_nodups_wids['sample_id'].map(mapid)
+    df_nodups_wids['GP2ID'] = [f'{study_code}_{i:06}' for i in df_nodups_wids.uid_idx]
+    df_nodups_wids['uid_idx_cumcount'] = df_nodups_wids.groupby('GP2ID').cumcount() + 1
+    df_nodups_wids['GP2sampleID'] = df_nodups_wids.GP2ID + '_s' + df_nodups_wids.uid_idx_cumcount.astype('str')
+    df_nodups_wids['SampleRepNo'] = 's' + df_nodups_wids.uid_idx_cumcount.astype('str')
+    df_nodups_wids.drop(['uid_idx','uid_idx_cumcount'], axis = 1, inplace = True)
 
+    if df_dups.shape[0]>0:
+        df_newids = pd.concat([df_dups_wids, df_nodups_wids])
+    else:
+        df_newids = df_nodups_wids
+    
+    return(df_newids)
 
 def assign_unique_gp2clinicalids(df, clinicalid_subset):
 
@@ -83,6 +124,20 @@ def assign_unique_gp2clinicalids(df, clinicalid_subset):
     getnewidrows = df.loc[index_modify].copy()
     return (getnewidrows)
 
+
+# def getgp2ids(df_updateids, n, uids, study_code):
+#     mapid = {}
+#     for uid in uids:
+#         mapid[uid]= n
+#         n += 1
+#     mydf = df_updateids.copy()
+#     mydf['uid_idx'] = mydf['sample_id'].map(mapid)
+#     mydf['GP2ID'] = [f'{study_code}_{i:06}' for i in mydf.uid_idx]
+#     mydf['uid_idx_cumcount'] = mydf.groupby('GP2ID').cumcount() + 1
+#     mydf['GP2sampleID'] = mydf.GP2ID + '_s' + mydf.uid_idx_cumcount.astype('str')
+#     mydf['SampleRepNo'] = 's' + mydf.uid_idx_cumcount.astype('str')
+#     mydf.drop(['uid_idx','uid_idx_cumcount'], axis = 1, inplace = True)
+#     return(mydf)
 
 
 # @st.cache
