23fdb047bee5dfa3d04e646cdcb69527f3f0fc25
AMCalejandro <alejandro.carrasco.20@ucl.ac.uk>
2023-09-03 16:00:54 -0400

    Adding session states vars for sm and clinical data

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
 import streamlit as st
 from multiapp import MultiApp
-from apps import home, data_checking, uploadSystem, data_visualization
+from apps import home, data_checking, clinical_qc, uploadSystem, data_visualization
 
 st.set_page_config(layout="wide")
 
-# Initialise dfqc in session state to move across tabs
-if 'allqc' not in st.session_state:
-    st.session_state['allqc'] = None
 if 'smqc' not in st.session_state:
     st.session_state['smqc'] = None
 if 'clinqc' not in st.session_state:
     st.session_state['clinqc'] = None
-if 'dct_tmplt' not in st.session_state:
-    st.session_state['dct_tmplt'] = None
+if 'master_get' not in st.session_state:
+    st.session_state['master_get'] = None
+if 'all_ids' not in st.session_state:
+    st.session_state['all_ids'] = None
 
 app = MultiApp()
 st.markdown(""" 
@@ -21,7 +20,8 @@ st.markdown("""
 )
 
 app.add_app("Home", home.app)
-app.add_app("QC", data_checking.app)
+app.add_app("sample manifest", data_checking.app)
+app.add_app("clinical data", clinical_qc.app)
 app.add_app("Upload", uploadSystem.app)
 app.add_app("Visualization", data_visualization.app)
 # Run the main app
